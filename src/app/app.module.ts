8cc5f4939ffec16df93a37cd871d5b83f5878982
Rafael V. de Oliveira <elrafael123@gmail.com>
2021-12-15 17:06:38 +0000

    layout and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { CartBadgeComponent } from './cart-badge/cart-badge.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatIconModule} from '@angular/material/icon';
+import { StoreModule } from '@ngrx/store';
+import { ShopReducer } from 'src/reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { ShopEffects } from 'src/effects';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CartComponent,
+    CartBadgeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatIconModule,
+    StoreModule.forRoot( { shop: ShopReducer }),
+    EffectsModule.forRoot([ShopEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
