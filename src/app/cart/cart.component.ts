8cc12ad3984a0395e3d224250f7ebe8d662c3b37
roliveira-nowo <rafael_oliveira@ext.nowo.pt>
2021-12-17 22:12:22 +0000

    empty cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { getCart, getProducts } from 'src/app/shared/selectors';
+import { Cart } from '../interfaces/cart.interface';
 import { Product } from '../interfaces/product.interface';
 
 @Component({
@@ -11,17 +12,23 @@ import { Product } from '../interfaces/product.interface';
 })
 export class CartComponent implements OnInit {
 
-  public cart$: Observable<Product[]>;
+  public cart$: Observable<Cart>;
 
-  displayedColumns: string[] = ['name', 'price', 'description'];
+  displayedColumns: string[] = ['image', 'name', 'price', 'quantity'];
   dataSource: any
+
+  emptyCart = false
 
   constructor(private readonly _store: Store) { }
 
   ngOnInit(): void {
     this.cart$ = this._store.select(getCart)
-    this._store.select(getCart).subscribe(products => {
-      this.dataSource = products
+    this._store.select(getCart).subscribe(cart => {
+      if ( cart ) {
+        this.dataSource = cart.products
+      } else {
+        this.emptyCart = true
+      }
     })
   }
 
