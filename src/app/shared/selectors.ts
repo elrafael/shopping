03e852327140935f4e1be5cba009480a76afad99
roliveira-nowo <rafael_oliveira@ext.nowo.pt>
2021-12-17 22:12:52 +0000

    cart total

--- src/app/shared/selectors.ts
+++ src/app/shared/selectors.ts
@@ -3,5 +3,5 @@ import { State } from "./reducer";
 
 export const getState = createFeatureSelector<State>('shop');
 export const getCart = createSelector(getState, state => state.cart)
-export const getCartTotal = createSelector(getState, state => state.cart.length)
+export const getCartTotal = createSelector(getState, state => (state.cart) ? state.cart.total : 0)
 export const getProducts = createSelector(getState, state => state.items)
