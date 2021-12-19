4c77041cf0b79081ba3d6d5021fa72000e2b84de
Rafael V. de Oliveira <elrafael123@gmail.com>
2021-12-19 13:04:04 +0000

    opt code

--- src/app/shared/reducer.ts
+++ src/app/shared/reducer.ts
@@ -1,6 +1,7 @@
 import { ActionsUnion, ActionTypes } from "./action";
 import { Product } from "../interfaces/product.interface";
 import { Cart, ProductsInCart } from "../interfaces/cart.interface";
+import { cloneObj, formatCart } from "./functions";
 export interface State {
   items?: Product[];
   cart?: Cart;
@@ -10,6 +11,9 @@ export const initialState: State = {
   cart: undefined
 };
 export function ShopReducer(state = initialState, action: ActionsUnion) {
+  let stateCart = state.cart
+  let myCart: Cart;
+  let total = 0;
   switch (action.type) {
     case ActionTypes.LoadSuccess:
       return {
@@ -18,81 +22,93 @@ export function ShopReducer(state = initialState, action: ActionsUnion) {
       };
 
     case ActionTypes.Add:
-      let cart: Cart
-      let total = 1
-      let value = 0
-      let quantity = 1
-      const myCart = state.cart
-
-      if ( myCart === undefined ) {
-        value = action.payload.price
+      if ( stateCart === undefined ) {
         const productsInCart: ProductsInCart = {
-          quantity,
+          quantity: 1,
           product: action.payload
         }
-        cart = {
+        myCart = {
           products: [productsInCart],
-          value,
+          value: action.payload.price,
           discount: 0,
           isPercent: false,
           total: 1
         }
       } else {
-        total = myCart.total + 1
-        value = myCart.value + action.payload.price
+        myCart = cloneObj(state.cart)
 
-
-        const find = myCart.products.find(product => product.product.id === action.payload.id)
-        if ( find ) {
-          quantity = find.quantity + 1
+        if ( myCart.products.length === 0 ) {
+          const productInCart: ProductsInCart = {
+            quantity: 1,
+            product: action.payload
+          }
+          myCart.products.push(productInCart)
+        } else {
+          myCart.products.map(cart => {
+            if ( cart.product.id === action.payload.id) {
+              cart.quantity += 1
+            } else {
+              const productInCart: ProductsInCart = {
+                quantity: 1,
+                product: action.payload
+              }
+              myCart.products.push(productInCart)
+            }
+          })
         }
 
-        const productsInCart: ProductsInCart = {
-          quantity,
-          product: action.payload
-        }
-        const products = [ ...myCart.products, productsInCart]
-        const unique = [...new Map(products.map(v => [v.product.id, v])).values()]
-
-
-        cart = {
-          products: unique,
-          value,
-          discount: 0,
-          isPercent: false,
-          total
-        }
-
-
+        myCart.total += 1
+        myCart.value += action.payload.price
       }
+      myCart = formatCart(myCart)
 
       return {
         ...state,
-        cart: cart
+        cart: myCart
       };
 
     case ActionTypes.Remove:
-      const products = state.cart.products.filter((item: ProductsInCart) => item.product.id !== action.payload.id)
       let valueRemoved = 0
-      products.map(product => {
+      myCart = cloneObj(state.cart)
+      myCart.products.filter((item: ProductsInCart) => item.product.id !== action.payload.id).map(product => {
         valueRemoved += product.product.price * product.quantity
       })
-      const cartRemoved: Cart = {
-        discount: state.cart.discount,
-        products: products,
-        value: valueRemoved,
-        isPercent: state.cart.isPercent,
-        total: (state.cart.total > 0) ? state.cart.total - 1 : 0
-      }
+      myCart.value = valueRemoved;
+      myCart.total = (state.cart.total > 0) ? state.cart.total - 1 : 0
+      myCart = formatCart(myCart)
+
       return {
         ...state,
-        cart: cartRemoved
-        // cart: state.cart
+        cart: myCart
       };
 
     case ActionTypes.IncreaseQuantity:
+      myCart = cloneObj(state.cart)
+      myCart.products.map(el => {
+        if ( el.product.id === action.payload.id ) {
+          el.quantity += 1
+        }
+        total += el.quantity * el.product.price
+      })
+      myCart.value = total
+      myCart = formatCart(myCart)
       return {
-        ...state
+        ...state,
+        cart: myCart
+      }
+    case ActionTypes.DecreaseQuantity:
+      myCart = cloneObj(state.cart)
+      myCart.products.map(el => {
+        if ( el.product.id === action.payload.id ) {
+          el.quantity -= (el.quantity > 0 ) ? 1 : el.quantity
+        }
+        total += el.quantity * el.product.price
+      })
+      myCart.value = total
+      myCart = formatCart(myCart)
+      return {
+        ...state,
+        cart: myCart
       }
     default:
       return { ...state };
